9b1cf6246e2a3c284bd53913b0b508903f1faa98
gofreeil <yahavanter@gmail.com>
2026-06-18 00:37:53 +0300

    Configure CORS for Vercel domain, enable proxy mode, set public URL

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,11 +3,29 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://community-il.duckdns.org'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://community-il.duckdns.org'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://localhost:4173'],
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:4173',
+        'https://community-il.vercel.app',
+        /^https:\/\/community-il-.*\.vercel\.app$/, // Vercel preview deployments
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
     },
